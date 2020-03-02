26d80cc532e7bd7ed12661421abf3f62a5b1cb2d
703Pvrtcx <kasaokapapkies@gmail.com>
2020-03-02 15:04:44 +0200

    RGB Blink Project Added
    Arduino Projects to be uploaded Here

---
+++ Blink_RGB.ino
@@ -0,0 +1,33 @@
+int redPin = 8;
+int greenPin = 9;
+int bluePin = 10;
+
+
+long maximum=1000;
+long prev =0;
+long myTimer;
+
+void setup()
+{
+  pinMode(redPin,OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  pinMode(bluePin,OUTPUT);
+    
+}
+void loop()
+{
+
+ myTimer=millis();
+  if(myTimer-prev>=maximum)
+  {
+    prev=myTimer;
+  
+  colorRGB(random(0,255),random(0,255),random(0,255));
+  }
+}
+void colorRGB(int red,int green,int blue)
+{   
+ analogWrite(redPin,constrain(red,0,255));
+ analogWrite(greenPin,constrain(green,0,255));
+ analogWrite(bluePin,constrain(blue,0,255)); 
+}
